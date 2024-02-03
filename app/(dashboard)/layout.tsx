2d63020a1673fd046fe230c0415167bd0563c99a
Sourjyo Deb <58564906+zorjo@users.noreply.github.com>
2024-02-03 22:26:07 +0530

    changed main dashboard page to include sidebar

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -6,9 +6,57 @@ export default function DashboardLayout({
   }) {
     return (
         <div className="h-full relative">
-        <div className="hidden h-full md:flex md:flex-col md:fixed md:inset-y-0 z-[80] md:w-72 bg-gray-900 fg-white-300">Hello sidebar <Navbar/></div>
-        <main className="md:pl-72 pb-10">{children}</main>
+   {/*   <div className="hidden h-full md:flex md:flex-col md:fixed md:inset-y-0 z-[80] md:w-72 bg-gray-900 fg-white-300">Hello jack<Navbar/></div>*/}
+  
+   <div className="grid min-h-screen grid-cols-[auto_1fr] justify-center gap-4 overflow-hidden p-4">
+        <aside className="flex h-[calc(100vh_-_2rem)] w-60 flex-col items-center  bg-slate-500 py-10">
+          
+          <ul className="space-y-2 font-medium">
+         <li>
+            <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+               <svg className="w-5 h-5 text-white-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
+                  <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z"/>
+                  <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z"/>
+               </svg>
+               <span className="ml-3 text-base font-semibold">ChatBot</span>
+            </a>
+         </li>
+         <li>
+            <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+               <svg className="w-5 h-5 text-white-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
+                  <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z"/>
+                  <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z"/>
+               </svg>
+               <span className="ml-3 text-base font-semibold">Image Generation</span>
+            </a>
+         </li>
+         <li>
+            <a href="#" className="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
+               <svg className="w-5 h-5 text-white-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
+                  <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z"/>
+                  <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z"/>
+               </svg>
+               <span className="ml-3 text-base font-semibold">QR Code Generation</span>
+            </a>
+         </li>
+
+         </ul>
+          
+        </aside>
+
+        <div className="h-[calc(100vh_-_2rem)] w-full overflow-y-scroll">
+          <input
+            type="text"
+            placeholder="search"
+            className="mb-2 w-full bg-slate-100"
+          />
+          <main className="min-h-screen w-full bg-slate-300 p-4">
+            <h1>Main content</h1>
+            {children}
+          </main>
         </div>
+      </div>
+      </div>
     )
   }
 /*
